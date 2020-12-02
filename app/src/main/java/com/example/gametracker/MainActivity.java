d4561be36d288b844fba5fce81038016b406ddf0
Andrew Cline <ancline@csumb.edu>
2020-12-02 06:03:13 -0800

    Initial commit

---
+++ app/src/main/java/com/example/gametracker/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.gametracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mLoginButton;
+    Button mCreateAccountButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLoginButton = findViewById(R.id.loginButton);
+        mCreateAccountButton = findViewById(R.id.createAccountButton);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        mCreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
